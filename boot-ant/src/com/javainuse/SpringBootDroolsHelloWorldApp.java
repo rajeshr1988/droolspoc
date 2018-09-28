0fa2bd5081c3e01468b1224c81654dc8f9b5592c
rajesh raj <rajeshr1988@gmail.com>
2018-09-28 10:16:50 +0530

    final

--- boot-ant/src/com/javainuse/SpringBootDroolsHelloWorldApp.java
+++ boot-ant/src/com/javainuse/SpringBootDroolsHelloWorldApp.java
@@ -1,18 +1,22 @@
 package com.javainuse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDroolsHelloWorldApp {
+public class SpringBootDroolsHelloWorldApp extends SpringBootServletInitializer {
 	 //private static final Logger logger = LoggerFactory.getLogger(SpringBootDroolsHelloWorldApp.class);
 	public static void main(String[] args) {
 		
 		SpringApplication.run(SpringBootDroolsHelloWorldApp.class, args);
 
 	}
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SpringBootDroolsHelloWorldApp.class);
+	    }
 
 	/*@Bean
 	public KieContainer kieContainer() {
